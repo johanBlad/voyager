61130085c231cfed9f394b4bf874546ac1d1dbe3
johanBlad <johan.blad@hotmail.se>
2021-01-24 21:15:02 +0100

    Initial commit

---
+++ src/main/java/com/jblad/voyager/VoyagerApplication.java
@@ -0,0 +1,13 @@
+package com.jblad.voyager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoyagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jblad/voyager/VoyagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jblad.voyager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
